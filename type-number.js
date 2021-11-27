94762789dabd2e3583013dcf76f97b570000d833
Junior Medeiros <juniormedeirosluce@gmail.com>
2021-11-27 14:51:32 -0300

    Exemplos da aula um de JavaScript

---
+++ type-number.js
@@ -0,0 +1,21 @@
+//Tipo número
+
+const numeroUm = 2;
+const numeroDois = 4;
+const operadorDeNumeros = numeroUm + numeroDois;
+
+console.log (operadorDeNumeros);
+
+//Numeros de ponto flutuante
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+const operadorMatematico = numeroUm * numeroPontoFlutuante;
+
+console.log(operadorMatematico);   
+
+//NaN = Not a Number
+
+const meuNome = "Junior";
+const operador = meuNome * numeroUm;
+
+console.log(operador);
---
+++ type-string.js
@@ -0,0 +1,13 @@
+const meuPrimeiroNome = "Junior";
+const meuSobrenome = "Medeiros";
+const nomeCompleto = meuPrimeiroNome + meuSobrenome;
+
+console.log ("Meu nome é:", meuPrimeiroNome, meuSobrenome);
+
+const olaMundo = 'Olá meu "mundo"';
+
+console.log(olaMundo);
+
+//Concetenação
+console.log(meuPrimeiroNome + meuSobrenome);
+console.log (nomeCompleto);
